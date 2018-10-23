4957135eb51759b98797a8c0a89070bea700e182
Aaron White <awhite9211@gmail.com>
2018-10-23 11:25:28 -0400

    updated for active workflow 1.2

--- footer.php
+++ footer.php
@@ -1,40 +1,12 @@
-<footer class="site-footer bg-dark container-responsive">
-
-  <!-- div class="row">
-      <?php if(is_active_sidebar('footer-widget-area')): ?>
-      <div class="row border-bottom pt-5 pb-4" id="footer" role="navigation">
-        <?php dynamic_sidebar('footer-widget-area'); ?>
-      </div>
-      <?php endif; ?>
-  </div -->
-  <div class="row">
-    <div class="col-md-12">
-   
-    </div>
-  </div>
-  <div class="row">
-    <div class=' col-md-4 col-sm-12'>
-        logo
-        <p class="text-center">&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p>
-
-    </div>
-    <div class=' col-md-8 col-sm-12 '>
-    <?php
-      wp_nav_menu( array(
-        'theme_location'  => 'navbar-top',
-        'container'       => false,
-        'menu_class'      => '',
-        'fallback_cb'     => '__return_false',
-        'items_wrap'      => '<ul id="%1$s" class="navbar-top menu-inline mr-auto  %2$s">%3$s</ul>',
-        'depth'           => 1,
-        'walker'          => new b4st_walker_nav_menu()
-      ) );
-    ?>
-    </div>
-  </div>
-
-  </div>
-
+<footer class="site-footer">
+	<div class="container-responsive">
+		<div class="row">
+			<div class="col-12">
+				<a class="footer-brand" href="<?php echo esc_url( home_url('/') ); ?>"><img src="<?php echo esc_url( get_template_directory_uri('/') ); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>" class="logo"></a>
+				<?php dynamic_sidebar( 'footer-widget-area' ); ?>
+			</div>
+		</div>
+	</div>
 </footer>
 
 <?php wp_footer(); ?>
--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,14 +1,3 @@
-
-jQuery(document).ready(function($){
-
-  $(window).scroll(function() {
-      var scrollPos = $(window).scrollTop();
-    var topMargin = parseInt($('.site-header .header-offset').css('paddingTop'));
-
-    // Main Nav shrinking/full-height.
-    // WARNING: affix *must not* be on the same element as *position: sticky*
-    $('.site-header > .container-responsive').toggleClass('affix', (scrollPos > topMargin));
-
-  });
+jQuery(document).ready( function($) {
 
 });
